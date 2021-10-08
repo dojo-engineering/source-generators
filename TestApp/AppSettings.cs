cbf6093581075e1c507755cc9b7b1848467737e2
Alex Malafeev <alex.malafeev@paymentsense.com>
2021-10-08 18:18:09 +0100

    test app

---
+++ TestApp/AppSettings.cs
@@ -0,0 +1,9 @@
+using Dojo.Generators.Abstractions;
+
+namespace ClassLibrary1
+{
+    [AutoAppSettings("appsettings.json")]
+    public partial class AppSettings
+    {
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using Dojo.Generators;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ClassLibrary1
+{
+    [AutoException]
+    public partial class MyException
+    {
+    }
+
+    public partial class GcpSettings: IGcpSettings
+    {
+        public void Foo()
+        {
+            Console.WriteLine(this.ProjectId);
+        }
+    }
+
+    public interface IGcpSettings
+    {
+        string ProjectId { get; }
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("/Users/alexmalafeev/paymentsense/source-generators/TestApp/appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            AppSettings apps = new AppSettings(configuration);
+            
+            apps.Gcp.Foo();
+            throw new MyException();
+        }
+    }
+}
