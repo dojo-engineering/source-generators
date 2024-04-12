9205431648d8eddd3e7d2a9d93c9b0ace3382280
GabrielBoceanu <boceanugabriel@gmail.com>
2024-04-12 20:19:29 +0100

    disable git workflows and check in code.

---
+++ Dojo.OpenApiGenerator/Exceptions/InvalidOpenApiSchemaException.cs
@@ -0,0 +1,28 @@
+﻿using SharpYaml.Schemas;
+using System;
+
+namespace Dojo.OpenApiGenerator.Exceptions
+{
+    public class InvalidOpenApiSchemaException : Exception
+    {
+        public InvalidOpenApiSchemaException()
+        {
+        }
+
+        public InvalidOpenApiSchemaException(string message) : base(message)
+        {
+        }
+
+        public InvalidOpenApiSchemaException(string message, string apiSchemaFile) : base($"{message} from Api File: {apiSchemaFile}")
+        {
+        }
+
+        public InvalidOpenApiSchemaException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public InvalidOpenApiSchemaException(string message, Exception innerException, string apiSchemaFile) : base($"{message} from Api File: {apiSchemaFile}", innerException)
+        {
+        }
+    }
+}
