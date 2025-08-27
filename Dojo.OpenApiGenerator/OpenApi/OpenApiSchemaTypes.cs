e7115945695745c9af238ed3bc67e2fc7396b5d1
Gabriel Boceanu <gabriel.boceanu@dojo.tech>
2025-08-27 21:45:26 +0100

    add support for float and doubles

--- Dojo.OpenApiGenerator/OpenApi/OpenApiSchemaTypes.cs
+++ Dojo.OpenApiGenerator/OpenApi/OpenApiSchemaTypes.cs
@@ -7,5 +7,6 @@
         public const string Integer = "integer";
         public const string Array = "array";
         public const string Boolean = "boolean";
+        public const string Number = "number";
     }
 }
